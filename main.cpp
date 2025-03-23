40836b834fe5357539b13b774906065f92f598dc
ezhowww <ezhoff.dmitriy@gmail.com>
2025-03-23 20:47:40 +0300

    start work on communicator

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-#include "Communicator.hpp"
+#include <cppSwarmLib/Logger.hpp>
 
 int main() {
-  Communicator comm;
-  comm.start();
-
-  char b;
-  do {
-    comm.send_message("Hello from Device!");
-    std::cin >> b;
-  } while (b != 'b');
-
-  std::cin.get();
-  comm.stop();
-  return 0;
+  swarm::BaseLogger l1("Swarm Logger 1");
+  swarm::BaseLogger l2("Swarm Logger 2");
+  swarm::BaseLogger l3("Swarm Logger 3");
+  l1.error("hello1", "Drone 1");
+  l1.info("hello1", "Drone 2");
+  l2.warning("bye1", "Drone 1");
+  l2.debug("bye2", "Drone 1");
+  l3.fatal("hehe", "Fedor");
 }
